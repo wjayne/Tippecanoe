dcb2037d0177874f0faff6590549f04deffe8310
root <root@tippe-assesor.localdomain>
2017-05-16 16:14:03 +0000

    from server

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 from app import app
+from flask import Flask
 import os
-app.run(threaded=True, debug=True)
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+
+#app = Flask(__name__)
+
+#@app.route("/")
+#def index():
+#	return "Hello world!"
+port = int(os.environ.get('PORT', 8000))
+if __name__ == "__main__":
+	app.run(threaded=True, port=port)
+#app.run(threaded=True, debug=True)
+#port = int(os.environ.get('PORT', 5000))
+#app.run(host='0.0.0.0', port=port)
 
 #comment for testing purpose
